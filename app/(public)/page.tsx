2d3d3e13f3c3d6a71a1a91622bdf8067665adc23
isabellafiuza <isabellafiuza0210@gmail.com>
2026-04-06 21:20:00 -0300

    Update page.tsx

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,4 +1,5 @@
-export const revalidate = 60
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 import type { Metadata } from 'next'
 import Link from 'next/link'
 import { prisma } from '@/lib/prisma'
